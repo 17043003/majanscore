aa487d57af8b54c2380bd0d9140e45fca9acdb98
17043003 <ishizuka533725@gmail.com>
2021-06-08 00:40:20 +0900

    Add: when register user exists, return user id and not register

--- src/infrastructure/UserRepositoryImpl.kt
+++ src/infrastructure/UserRepositoryImpl.kt
@@ -12,7 +12,13 @@ class UserRepositoryImpl: UserRepository {
 
         return transaction {
             addLogger(StdOutSqlLogger)
-            
+
+            // return user id when user is already registered
+            val userRow = UserTable.select{ UserTable.email like email }.singleOrNull()
+            if(userRow != null){
+                return@transaction userRow[UserTable.id]
+            }
+
             UserTable.insert {
                 it[this.name] = name
                 it[this.email] = email
@@ -29,9 +35,19 @@ class UserRepositoryImpl: UserRepository {
         }
     }
 
+    override fun findUser(email: String): User {
+        DataBaseUtil.connectDatabase()
+
+        return transaction {
+            val userRow = UserTable.select{ UserTable.email eq email }.single()
+            User(userRow[UserTable.id], userRow[UserTable.name], userRow[UserTable.email])
+        }
+    }
+
     object UserTable: Table("user"){
         val id = integer("id").autoIncrement()
         val name = varchar("name", 32)
-        val email = varchar("email", 128)
+        val email = varchar("email", 128).uniqueIndex()
+        override val primaryKey = PrimaryKey(id)
     }
 }
